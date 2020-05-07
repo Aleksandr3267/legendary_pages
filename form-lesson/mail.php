c615391f0acf9426026bfac5b254587f687d61b9
Aleksandr3267 <52033027+Aleksandr3267@users.noreply.github.com>
2020-05-07 19:00:50 +0600

    testform
    form

--- form-lesson/mail.php
+++ form-lesson/mail.php
@@ -13,12 +13,12 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'sanya.pyatkov.89@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'ASBMW326iSANYA_30_'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'ivan.ivanovich.9000@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'silver326'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('sanya.pyatkov.89@mail.ru'); // от кого будет уходить письмо?
+$mail->setFrom('ivan.ivanovich.9000@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('sanya.pyatkov.89@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
