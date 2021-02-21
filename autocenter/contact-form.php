387c772c4b912e6d972538ba196373e39a3e75b0
Aleksander <sanya.pyatkov.89@mail.ru>
2021-02-21 16:12:37 +0600

    autocenter
    autocenter

---
+++ autocenter/contact-form.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$message = htmlspecialchars($_POST["message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+
+/* Ваш адрес и тема сообщения */
+$address = "geniy.kg@inbox.ru";
+$sub = "Сообщение с сайта genius";
+
+/* Формат письма */
+$mes = "Сообщение с сайта genius.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Телефон отправителя: $tel
+Текст сообщения:
+$message";
+
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from = "Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 5; URL=http://genius.kg');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу Гений</body>';}
+else {
+	header('Refresh: 5; URL=http://genius.kg');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу Гений</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama чем-то заполнено */
+?>
---
+++ autocenter/js/script.js
@@ -0,0 +1,65 @@
+'use strict';
+// якорь
+$(document).ready(function(){
+    $("#main-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+// burger
+$(document).ready(function () {
+    $('.burder_menu').click(function (event) {
+        $('.burder_menu, #main-menu, #main-menu-news').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+$('.link').on('click', function(){
+    $('.burder_menu, #main-menu, #main-menu-news').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+
+
+// slider
+new Swiper('.image_slider', {
+    // pagination: {
+    //     el: '.swiper-pagination',
+
+    // type: 'progressbar'
+    // },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+
+        draggable: true
+    },
+    autoHeight: true,
+
+    slidesPerView:3,
+    spacebetween:30,
+
+    sledesPerGroup: 1,
+
+    // for scroll on curcle
+    mousewheel: {
+        // sensitivity 
+        sensitivity:1,
+
+        eventsTarget: ".image_slider"
+    },
+
+    // adaptive
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        900: {
+            slidesPerView: 3,
+        }
+    },
+});
